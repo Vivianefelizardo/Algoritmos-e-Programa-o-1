45b00158233aaae02a77ae3919690677dc224a23
Vivianefelizardo <gomesviviane405@gmail.com>
2026-09-16 11:01:37 -0300

    Add files via upload

---
+++ Aula4/menu.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class menu{
+    public static void main(String[]args){
+        System.out.println("======MEnu de opções======");
+        System.out.println("1 - Cadastrar produtos");
+        System.out.println("2 - Listas de produtos");
+        System.out.println("3 - Sair do sistema");
+        System.out.println("=======Escolha uma opção=======");
+
+        Scanner entrada = new Scanner(System.in);
+
+        int menu= entrada.nextInt();
+
+        switch(menu){
+            case 1:
+                System.out.println("Você escolheu o menu 1");
+                System.out.println("Que é a opção Cadastrar Produtos");
+                break;
+            case 2:
+                System.out.println("Você escolheu o menu 2");
+                System.out.println("Que é a opção Lista de Produtos");
+                break;
+            case 3:
+                System.out.println("Você escolheu o menu 3");
+                System.out.println("Que é a opção Sair do Sistema");
+                break;
+            default:
+                System.out.println("Item de menu inválido");
+
+        }
+
+        entrada.close();
+
+
+
+
+        
+    
+    }
+}
