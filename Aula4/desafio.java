b36f9a2168c276c4c0afaf275806e7b627e04405
Vivianefelizardo <gomesviviane405@gmail.com>
2026-09-16 08:56:01 -0300

    Update desafio.java

--- Aula4/desafio.java
+++ Aula4/desafio.java
@@ -6,20 +6,14 @@ public class desafio{
         Scanner entrada = new Scanner(System.in);
         int idade;
         String resposta;
-        boolean alfabetizado;
 
         System.out.println("Digite a sua idade: ");
         idade = entrada.nextInt();
 
-        System.out.println("Você é alfabetizado(a)? (S/N): ");
-        resposta = entrada.next();
-
-        alfabetizado = resposta.equalsIgnoreCase("S"); 
-
         if (idade < 16) {
             System.out.println("Não apta a votar.");
         }
-        else if (idade >= 18 && idade <= 70 && alfabetizado) {
+        else if (idade >= 18 && idade <= 70) {
             System.out.println("Apta a votar. O voto é obigatório!");
         }
         else {
