a35cfff74e2377cd4abb31ca9f625655623dbdd5
Vivianefelizardo <gomesviviane405@gmail.com>
2026-09-21 18:17:48 -0300

    Add files via upload

---
+++ aula5/exercicio1Aula5.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class exercicio1Aula5{
+    public static void main(String[] args) {
+
+        System.out.println("Digite um número de 1 - 7: ");
+
+        Scanner entrada = new Scanner(System.in);
+        int dia = entrada.nextInt();
+        switch(dia){
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda-feira");
+                break;
+            case 3:
+                System.out.println("Terça-feira");
+                break;
+            case 4:
+                System.out.println("Quarta-feira");
+                break;
+            case 5:
+                System.out.println("Quinta-feira");
+                break;
+            case 6:
+                System.out.println("Sexta-feira");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                break;
+            default:
+                System.out.println("Número inválido!");
+        }
+        entrada.close();
+}
+}
