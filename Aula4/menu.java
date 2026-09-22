8df1e82f8fac49fa50a9e02362c3b1325a328927
Vivianefelizardo <gomesviviane405@gmail.com>
2026-09-22 08:33:52 -0300

    Fix typo in menu title output

--- Aula4/menu.java
+++ Aula4/menu.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class menu{
     public static void main(String[]args){
-        System.out.println("======MEnu de opções======");
+        System.out.println("======Menu de opções======");
         System.out.println("1 - Cadastrar produtos");
         System.out.println("2 - Listas de produtos");
         System.out.println("3 - Sair do sistema");
